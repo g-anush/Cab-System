a01034d3eb024e748682af74c7515a2d8acd0066
gANUSH <anushgupta2001@gmail.com>
2024-03-28 22:55:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from './components/HomePage.js';
+import TitleBar from './components/TitleBar';
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <TitleBar/>
+    <HomePage/>
+   </>
+  );
+}
+
+export default App;
